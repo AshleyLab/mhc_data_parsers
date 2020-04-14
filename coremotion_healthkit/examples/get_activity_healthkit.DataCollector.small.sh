01059c10cfd258b0bbceed36593cca3d740c41bb
Anna Shcherbina <annashcherbina@gmail.com>
2020-04-14 10:41:39 -0700

    updated example

--- coremotion_healthkit/examples/get_activity_healthkit.DataCollector.small.sh
+++ coremotion_healthkit/examples/get_activity_healthkit.DataCollector.small.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 data_prefix=/oak/stanford/groups/euan/projects/mhc
 #DataCollector 
-python get_activity.py --tables $data_prefix/data/tables/cardiovascular-HealthKitDataCollector-v1.tsv \
+python ../get_activity.py --tables $data_prefix/data/tables/cardiovascular-HealthKitDataCollector-v1.tsv \
     --synapseCacheDir $data_prefix/data/synapseCache/ \
     --out_prefixes parsed_HealthKitData \
     --data_types health_kit_data_collector \
-    --subjects hk.datacollector.subjects.10
+    --subjects inputs/hk.datacollector.brandon 
